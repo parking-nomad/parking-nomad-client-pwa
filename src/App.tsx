33a3fbf24fee72c8e7e1f21e0c6f56970e77c70b
chacha912 <dkwkdnwk912@gmail.com>
2024-04-13 18:07:03 +0900

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import logo from '/icons/favicon-192x192.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Parking Nomad</h1>
+      <img src={logo} className="logo" alt="Parking Nomad" />
+    </>
+  )
+}
+
+export default App
