124ff5568819933cbe869c84c527e4fc9de1d55a
chacha912 <dkwkdnwk912@gmail.com>
2024-04-14 23:38:14 +0900

    Apply prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import logo from '/icons/favicon-192x192.png'
-import './App.css'
+import logo from '/icons/favicon-192x192.png';
+import './App.css';
 
 function App() {
-
   return (
     <>
       <h1>Parking Nomad</h1>
       <img src={logo} className="logo" alt="Parking Nomad" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
